eb574a44870ccab8b542b368ad420868fad46824
Pinal <pinalj1612@gmail.com>
2019-03-28 17:19:21 +0530

    Version 1.2.6
    Updated:
    
    1. readme
    2. Version numbers
    3. WC and WP tested up to values

--- custom-order-numbers-for-woocommerce/custom-order-numbers-for-woocommerce.php
+++ custom-order-numbers-for-woocommerce/custom-order-numbers-for-woocommerce.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Order Numbers for WooCommerce
 Plugin URI: https://www.tychesoftwares.com/store/premium-plugins/custom-order-numbers-woocommerce/
 Description: Custom order numbers for WooCommerce.
-Version: 1.2.5
+Version: 1.2.6
 Author: Tyche Softwares
 Author URI: https://www.tychesoftwares.com/
 Text Domain: custom-order-numbers-for-woocommerce
@@ -11,7 +11,7 @@ Domain Path: /langs
 Copyright: © 2018 Tyche Softwares
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
-WC tested up to: 3.5.0
+WC tested up to: 3.5.7
 */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
@@ -53,7 +53,7 @@ final class Alg_WC_Custom_Order_Numbers {
 	 * @var   string
 	 * @since 1.0.0
 	 */
-	public $version = '1.2.5';
+	public $version = '1.2.6';
 
 	/**
 	 * @var   Alg_WC_Custom_Order_Numbers The single instance of the class
