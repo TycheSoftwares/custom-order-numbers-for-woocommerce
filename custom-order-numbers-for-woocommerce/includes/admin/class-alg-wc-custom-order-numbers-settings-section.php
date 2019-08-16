c698fe37e0f8be0148b6cfc9bc099e5b9a6c560a
Pinal <pinalj1612@gmail.com>
2019-08-16 15:04:37 +0530

    PHP CS changes
    PHP CS changes

--- custom-order-numbers-for-woocommerce/includes/admin/class-alg-wc-custom-order-numbers-settings-section.php
+++ custom-order-numbers-for-woocommerce/includes/admin/class-alg-wc-custom-order-numbers-settings-section.php
@@ -5,74 +5,84 @@
  * @version 1.2.0
  * @since   1.0.0
  * @author  Tyche Softwares
+ * @package Custom-Order-Numbers-Lite
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
 
 if ( ! class_exists( 'Alg_WC_Custom_Order_Numbers_Settings_Section' ) ) :
 
-class Alg_WC_Custom_Order_Numbers_Settings_Section {
-
 	/**
-	 * Constructor.
-	 *
-	 * @version 1.0.0
-	 * @since   1.0.0
+	 * Settings class.
 	 */
-	function __construct() {
-		add_filter( 'woocommerce_get_sections_alg_wc_custom_order_numbers',              array( $this, 'settings_section' ) );
-		add_filter( 'woocommerce_get_settings_alg_wc_custom_order_numbers_' . $this->id, array( $this, 'get_settings' ), PHP_INT_MAX );
-		add_action( 'init',                                                              array( $this, 'add_settings_hook' ) );
-	}
+	class Alg_WC_Custom_Order_Numbers_Settings_Section {
 
-	/**
-	 * add_settings_hook.
-	 *
-	 * @version 1.0.0
-	 * @since   1.0.0
-	 */
-	function add_settings_hook() {
-		add_filter( 'alg_custom_order_numbers_settings_' . $this->id, array( $this, 'add_settings' ) );
-	}
+		/**
+		 * Constructor.
+		 *
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 */
+		public function __construct() {
+			add_filter( 'woocommerce_get_sections_alg_wc_custom_order_numbers', array( $this, 'settings_section' ) );
+			add_filter( 'woocommerce_get_settings_alg_wc_custom_order_numbers_' . $this->id, array( $this, 'get_settings' ), PHP_INT_MAX );
+			add_action( 'init', array( $this, 'add_settings_hook' ) );
+		}
 
-	/**
-	 * get_settings.
-	 *
-	 * @version 1.2.0
-	 * @since   1.0.0
-	 */
-	function get_settings() {
-		return array_merge( apply_filters( 'alg_custom_order_numbers_settings_' . $this->id, array() ), array(
-			array(
-				'title'     => __( 'Reset Settings', 'custom-order-numbers-for-woocommerce' ),
-				'type'      => 'title',
-				'id'        => 'alg_wc_custom_order_numbers_' . $this->id . '_reset_options',
-			),
-			array(
-				'title'     => __( 'Reset section settings', 'custom-order-numbers-for-woocommerce' ),
-				'desc'      => '<strong>' . __( 'Reset', 'custom-order-numbers-for-woocommerce' ) . '</strong>',
-				'id'        => 'alg_wc_custom_order_numbers_' . $this->id . '_reset',
-				'default'   => 'no',
-				'type'      => 'checkbox',
-			),
-			array(
-				'type'      => 'sectionend',
-				'id'        => 'alg_wc_custom_order_numbers_' . $this->id . '_reset_options',
-			),
-		) );
-	}
+		/**
+		 * Add_settings_hook.
+		 *
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 */
+		public function add_settings_hook() {
+			add_filter( 'alg_custom_order_numbers_settings_' . $this->id, array( $this, 'add_settings' ) );
+		}
 
-	/**
-	 * settings_section.
-	 *
-	 * @version 1.0.0
-	 * @since   1.0.0
-	 */
-	function settings_section( $sections ) {
-		$sections[ $this->id ] = $this->desc;
-		return $sections;
-	}
+		/**
+		 * Get_settings.
+		 *
+		 * @version 1.2.0
+		 * @since   1.0.0
+		 */
+		public function get_settings() {
+			return array_merge(
+				apply_filters( 'alg_custom_order_numbers_settings_' . $this->id, array() ),
+				array(
+					array(
+						'title' => __( 'Reset Settings', 'custom-order-numbers-for-woocommerce' ),
+						'type'  => 'title',
+						'id'    => 'alg_wc_custom_order_numbers_' . $this->id . '_reset_options',
+					),
+					array(
+						'title'   => __( 'Reset section settings', 'custom-order-numbers-for-woocommerce' ),
+						'desc'    => '<strong>' . __( 'Reset', 'custom-order-numbers-for-woocommerce' ) . '</strong>',
+						'id'      => 'alg_wc_custom_order_numbers_' . $this->id . '_reset',
+						'default' => 'no',
+						'type'    => 'checkbox',
+					),
+					array(
+						'type' => 'sectionend',
+						'id'   => 'alg_wc_custom_order_numbers_' . $this->id . '_reset_options',
+					),
+				)
+			);
+		}
 
-}
+		/**
+		 * Settings_section.
+		 *
+		 * @param array $sections - List of sections (ID & Desc).
+		 * @version 1.0.0
+		 * @since   1.0.0
+		 */
+		public function settings_section( $sections ) {
+			$sections[ $this->id ] = $this->desc;
+			return $sections;
+		}
+
+	}
 
 endif;
--- custom-order-numbers-for-woocommerce/uninstall.php
+++ custom-order-numbers-for-woocommerce/uninstall.php
@@ -7,37 +7,38 @@
  * @author      Tyche Softwares
  * @category    Core
  * @version     1.2.6
+ * @package     Custom-Order-Numbers-Lite
  */
-    
+
 if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
 // check if the Pro version file is present. If yes, do not delete any settings irrespective of whether the plugin is active or no.
-if( file_exists( WP_PLUGIN_DIR . '/custom-order-numbers-for-woocommerce-pro/custom-order-numbers-for-woocommerce-pro.php' ) ) {
+if ( file_exists( WP_PLUGIN_DIR . '/custom-order-numbers-for-woocommerce-pro/custom-order-numbers-for-woocommerce-pro.php' ) ) {
 	return;
 }
 
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
 /**
  * Delete the data for the WordPress Multisite.
  */
 if ( is_multisite() ) {
-	
+
 	$con_blog_list = get_sites();
 
-    foreach( $con_blog_list as $con_blog_list_key => $con_blog_list_value ) {
+	foreach ( $con_blog_list as $con_blog_list_key => $con_blog_list_value ) {
 
 
-    	$con_blog_id = $con_blog_list_value->blog_id;
-    	
-    	/**
-    	 * It indicates the sub site id.
-    	 */
-    	$con_multisite_prefix = $con_blog_id > 1 ? $wpdb->prefix . "$con_blog_id_" : $wpdb->prefix;
-    	
-    	// General Settings
+		$con_blog_id = $con_blog_list_value->blog_id;
+
+		/**
+		 * It indicates the sub site id.
+		 */
+		$con_multisite_prefix = $con_blog_id > 1 ? $wpdb->prefix . "$con_blog_id_" : $wpdb->prefix;
+
+		// General Settings.
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_enabled' );
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_counter_type' );
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_counter' );
@@ -54,18 +55,18 @@ if ( is_multisite() ) {
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_manual_enabled' );
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_hide_menu_for_roles' );
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_hide_tab_for_roles' );
-		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers__reset' );	
+		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers__reset' );
 		delete_blog_option( $con_blog_id, 'alg_wc_custom_order_numbers_counter_previous_order_date' );
-		// License
+		// License.
 		delete_blog_option( $con_blog_id, 'edd_license_key_con' );
 
-		// Version Number
+		// Version Number.
 		delete_blog_option( $con_blog_id, 'alg_custom_order_numbers_version' );
 
 	}
-} else { 
+} else {
 
-	// General Settings
+	// General Settings.
 	delete_option( 'alg_wc_custom_order_numbers_enabled' );
 	delete_option( 'alg_wc_custom_order_numbers_counter_type' );
 	delete_option( 'alg_wc_custom_order_numbers_counter' );
@@ -84,14 +85,12 @@ if ( is_multisite() ) {
 	delete_option( 'alg_wc_custom_order_numbers_hide_tab_for_roles' );
 	delete_option( 'alg_wc_custom_order_numbers__reset' );
 	delete_option( 'alg_wc_custom_order_numbers_counter_previous_order_date' );
-	// License
+	// License.
 	delete_option( 'edd_license_key_con' );
 
-	// Version Number
+	// Version Number.
 	delete_option( 'alg_custom_order_numbers_version' );
 
 }
-// Clear any cached data that has been removed
+// Clear any cached data that has been removed.
 wp_cache_flush();
-
-?>
