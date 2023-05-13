0856e025aa933be63faaafd7e58f63f03ab5107a
chetna1510 <100338310+nidhitatosaniya@users.noreply.github.com>
2023-05-13 16:40:03 +0530

    fixed phpcs errors.

--- includes/js/con-dismiss-notice.js
+++ includes/js/con-dismiss-notice.js
@@ -5,24 +5,32 @@
  * @since 1.3.0
  */
 jQuery(document).ready(function($) {
-    jQuery( '.con-lite-success-message' ).on( 'click', '.notice-dismiss', function() {
-		console.log('here');
-		var data = {
-			admin_choice: 'dismissed',
-			security: con_dismiss_param.nonce,
-			action: 'alg_custom_order_numbers_admin_notice_dismiss'
-		};
-		jQuery.post( con_dismiss_param.ajax_url, data, function() {
-		});
-    });
+    jQuery( '.con-lite-success-message' ).on(
+		'click',
+		'.notice-dismiss',
+		function() {
+			console.log( 'here' );
+			var data = {
+				admin_choice: 'dismissed',
+				security: con_dismiss_param.nonce,
+				action: 'alg_custom_order_numbers_admin_notice_dismiss'
+			};
+			jQuery.post( con_dismiss_param.ajax_url, data, function() {
+			});
+    	}
+	);
 
-	jQuery( '.con-lite-meta-key-success-message' ).on( 'click', '.notice-dismiss', function() {
-		var data = {
-			alg_admin_choice: 'dismissed',
-			security: con_dismiss_param.nonce,
-			action: 'alg_custom_order_numbers_admin_meta_key_notice_dismiss'
-		};
-		jQuery.post( con_dismiss_param.ajax_url, data, function() {
-		});
-    });
+	jQuery( '.con-lite-meta-key-success-message' ).on(
+		'click',
+		'.notice-dismiss',
+		function() {
+			var data = {
+				alg_admin_choice: 'dismissed',
+				security: con_dismiss_param.nonce,
+				action: 'alg_custom_order_numbers_admin_meta_key_notice_dismiss'
+			};
+			jQuery.post( con_dismiss_param.ajax_url, data, function() {
+			});
+    	}
+	);
 });
