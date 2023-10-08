421bbf686b6ac6fbb570c811f394e9c9efe621ba
Daniel Winzen <daniel@danwin1210.de>
2023-10-08 09:51:13 +0200

    Lock row for update to prevent duplicate sequential order numbers

--- includes/class-alg-wc-custom-order-numbers-core.php
+++ includes/class-alg-wc-custom-order-numbers-core.php
@@ -1123,7 +1123,7 @@ if ( ! class_exists( 'Alg_WC_Custom_Order_Numbers_Core' ) ) :
 					global $wpdb;
 					$wpdb->query( 'START TRANSACTION' ); //phpcs:ignore
 					$wp_options_table = $wpdb->prefix . 'options';
-					$result_select    = $wpdb->get_row( $wpdb->prepare( 'SELECT * FROM `' . $wpdb->prefix . 'options` WHERE option_name = %s', 'alg_wc_custom_order_numbers_counter' ) ); //phpcs:ignore
+					$result_select    = $wpdb->get_row( $wpdb->prepare( 'SELECT * FROM `' . $wpdb->prefix . 'options` WHERE option_name = %s FOR UPDATE', 'alg_wc_custom_order_numbers_counter' ) ); //phpcs:ignore
 					if ( null !== $result_select ) {
 						$current_order_number     = $this->maybe_reset_sequential_counter( $result_select->option_value, $order_id );
 						$result_update            = $wpdb->update( // phpcs:ignore
